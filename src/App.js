0b4fe3061c9cfc447c8b43b4ca265f57ed7bbd37
franciscoG98 <franciscomanuel.gonzalez3@gmail.com>
2021-12-19 20:29:15 -0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      App works!
+      <h1>App works!</h1>
+      <hr />
+      <Router>
+        <Routes>
+          <Route path="/about" element={"Esta es la pagina de nosotros"} />
+          <Route exact path="/" element={"Homesick :/"} />
+        </Routes>
+      </Router>
     </div>
   );
 }
